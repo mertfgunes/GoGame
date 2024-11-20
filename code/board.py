cbcdfc5e73e12eb8a687688fd5da2e4a913525a6
MertFGunes <mertfaruk.ie@gmail.com>
2024-11-20 14:57:26 +0000

    simple import. also testing commiting.

--- code/board.py
+++ code/board.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QFrame
-from PyQt6.QtCore import Qt, QTimer, pyqtSignal
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QPoint
 from PyQt6.QtGui import QPainter, QColor, QBrush
 
 class Board(QFrame):  # base the board on a QFrame widget
