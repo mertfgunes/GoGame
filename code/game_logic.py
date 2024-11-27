5ba5be78d32b1b870aa82dca782ff0009f210b0d
MertFGunes <mertfaruk.ie@gmail.com>
2024-11-27 12:25:44 +0000

    possible move checker method has been added.

--- code/game_logic.py
+++ code/game_logic.py
@@ -5,3 +5,8 @@ class GoGame:
         self.board = [[0 for _ in range(board_size)] for _ in range(board_size)]
         self.current_player = 1  # 1 for black, 2 for white because black goes first.
 
+    def is_possible_move(self, x, y):
+        # check if the move is possible
+        return 0 <= x < self.board_size and 0 <= y < self.board_size and self.board[x][y] == 0
+
+
