adbbfcaf71c4370a1d2bef14333361e0883273f9
MertFGunes <mertfaruk.ie@gmail.com>
2024-11-27 12:24:27 +0000

    board for now and turns taken logic.

--- code/game_logic.py
+++ code/game_logic.py
@@ -1,4 +1,7 @@
-class GameLogic:
-    print("Game Logic Object Created")
-    # TODO add code here to manage the logic of your game
-    # mert
+class GoGame:
+    def __init__(self, board_size=7):
+        # this is temporarily since it is going to be adjustable in the future
+        self.board_size = board_size
+        self.board = [[0 for _ in range(board_size)] for _ in range(board_size)]
+        self.current_player = 1  # 1 for black, 2 for white because black goes first.
+
